1933ae382f9da3a74e6099b99cee3f37b833e52a
Victor25Brum <victoraugustobrum@gmail.com>
2023-12-19 15:03:53 -0300

    atualiza periodo

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,7 +4,7 @@ const AboutContainer = () => {
             <h2>Sobre</h2>
             <p>
                 Programador formado em 2019 pelo CEFET-MG/Timóteo no curso técnico de Desenvolvimento de Sistemas.
-                Atualmente cursando o 5° período de Sistemas de Informação pela Faculdade Doctum de Ipatinga.
+                Atualmente cursando o 6° período de Sistemas de Informação pela Faculdade Doctum de Ipatinga.
                 Com experiência em migração, desenvolvimento de relatórios e scripts de diferentes tipos de API, 
                 manutenção e segurança de servidores via AD (Active Directory), hardware e software.
             </p>
