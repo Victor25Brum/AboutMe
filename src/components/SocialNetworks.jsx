3010934768cb79d68f3970d233dfdd7aca3e8305
Victor25Brum <victoraugustobrum@gmail.com>
2024-10-16 15:03:39 -0300

    social_media

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -5,24 +5,19 @@ import '../styles/components/socialnetworks.sass';
 
 //array de objetos (icones)
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn/>},
-    { name: "github", icon: <FaGithub/>},
-    { name: "instagram", icon: <FaInstagram/>},
+    { name: "linkedin", icon: <FaLinkedinIn/>, url: "https://www.linkedin.com/in/victor-brum-ab536b165" },
+    { name: "github", icon: <FaGithub/>, url: "https://github.com/Victor25Brum" },
+    { name: "instagram", icon: <FaInstagram/>, url: "https://www.instagram.com/brum_7" },
 ];
 
 const SocialNetworks = () => {
     return (
         <section id="social-networks">
-        {/*percorrendo socialNetworks, pegando cada icone para ser exibido atraves da variavel network*/}
-            {
-                socialNetworks.map(
-                    (network) => (
-                        <a href="#" className="social-btn" id={network.name} key={network.name}>
-                            {network.icon}
-                        </a>
-                    )
-                )
-            }
+            {socialNetworks.map((network) => (
+                <a href={network.url} className="social-btn" id={network.name} key={network.name} target="_blank" rel="noopener noreferrer">
+                    {network.icon}
+                </a>
+            ))}
         </section>
     );
 }
